bef753c122a4a6a707db603d092bca5e7b0bd48c
muabow <pyop60@gmail.com>
2022-01-06 11:16:03 +0900

    modify example code

--- library/php/src_curl/example.php
+++ library/php/src_curl/example.php
@@ -1,15 +1,18 @@
 <?php
     include_once("./func_curl.php");
 
-    /* Make option info
+    /*
+        # Make option info
         auth 	 => CURLAUTH_BASIC, CURLAUTH_DIGEST, CURLAUTH_GSSNEGOTIATE, CURLAUTH_NTLM, CURLAUTH_ANY, CURLAUTH_ANYSAFE
         username => "username"
         password => "password"
     */
+    $str_target = "<Rest API Server>";  // e.g. http://192.168.1.99/api/status
+    $str_data	= "";	                // "" is GET method, other POST method
     $arr_opt	= array("auth" => CURLAUTH_DIGEST, "username" => "root", "password" => "root");
-    $str_data	= "";	// "" is GET method, other POST method
 
-    $response	= curl_request_data("<Rest API Server>", $str_data, $arr_opt);
+    // curl request
+    $response = curl_request_data($str_target, $str_data, $arr_opt);
 
     print_r(json_decode($response));
 ?>
